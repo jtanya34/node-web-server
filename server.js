076d8acb0ce665983823d44f57eea37f77e19d17
tanya <jtanya34@gmail.com>
2018-05-10 23:25:28 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express=require('express');
+const hbs=require('hbs');
+const fs=require('fs');
+
+var app=express();
+
+hbs.registerPartials(__dirname +'/views/partials')
+app.set('view engine','hbs');
+
+
+
+
+app.use((req,res,next)=>{
+	var now=new Date().toString();
+	var log=`${now}: ${req.method} ${req.url}`;
+
+	console.log(log);
+	fs.appendFile('server.log', log+'\n',(err)=>{
+		if(err){
+			console.log('Unable to append server to log')
+		}
+		next();
+	});
+});
+
+
+// app.use((req,res,next)=>{
+// //res.send('<h1>Hello Express</h1>');
+// res.render('maintainence.hbs');
+// });
+
+app.use(express.static(__dirname +'/public'));
+
+hbs.registerHelper('getCurrentYear',()=>{
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(text)=>{
+	return text.toUpperCase();
+});
+
+
+app.get('/',(req,res)=>{
+//res.send('<h1>Hello Express</h1>');
+res.render('Home.hbs',{
+	pageTitle:'Home Page',
+	welcomeMessage:'Welcome to my website',
+
+
+});
+});
+
+app.get('/about',(req,res)=>{
+	res.render('about.hbs',{
+pageTitle:'About Page',
+currentYear: new Date().getFullYear()
+
+	});
+});
+
+
+app.get('/bad',(req,res)=>{
+res.send({
+	errormessage:'Unable to handle data'
+	});
+});
+
+app.listen(3000);
